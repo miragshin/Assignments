61b186ae19eee81e687674304068eaab24094371
Catie Sapphire <>
2018-03-03 17:41:47 +0400

    as06_2

--- as06/src/Task1_flag.java
+++ as06/src/Task1_flag.java
@@ -11,33 +11,25 @@ import java.awt.*;
  * The half-moon must be drawn! Note that, the flag must the entire app window!
  */
 
-public class Task1_flag extends GraphicsProgram{
+public class Task1_flag extends GraphicsProgram {
 	private static final long serialVersionUID = 1L;
 	private static final Color AZ_BLUE = new Color(0, 181, 226);
 	private static final Color AZ_RED = new Color(239, 51, 64);
 	private static final Color AZ_GREEN = new Color(80, 158, 47);
 
-
-
-
-
-
-
-
 	public void run() {
 
-		// Less function calls
-		final double CANVAS_WIDTH = getWidth();
-		final double CANVAS_HEIGHT = getHeight();
-
-		GRect blueRect = new GRect(0, 0, getWidth(), getHeight() / 3);
-		GRect redRect = new GRect(0, getHeight() / 3, getWidth(), getHeight() / 3);
-		GRect greenRect = new GRect(0, getHeight() / 1.5, getWidth(), getHeight() / 3);
+		//Because there are too many function calls otherwise
+		final double WIDTH = getWidth();
+		final double HEIGHT = getHeight() / 3;
+		GRect blueRect = new GRect(0, 0, WIDTH, HEIGHT);
+		GRect redRect = new GRect(0, HEIGHT, WIDTH, HEIGHT);
+		GRect greenRect = new GRect(0, HEIGHT * 2, WIDTH, HEIGHT);
 		GOval outerMoon = new GOval(
-				(getWidth() - getWidth() / 3) / 2,
-				(getHeight() - getHeight() / 3) / 2,
-				getHeight() / 3,
-				getHeight() / 3
+				(WIDTH - WIDTH / 3) / 2,
+				(HEIGHT * 3 - HEIGHT) / 2,
+				HEIGHT,
+				HEIGHT
 		);
 
 		GOval innerMoon = new GOval(
@@ -47,42 +39,36 @@ public class Task1_flag extends GraphicsProgram{
 				outerMoon.getHeight() - 20
 		);
 
+		// Set the objects filled
 		blueRect.setFilled(true);
 		redRect.setFilled(true);
 		greenRect.setFilled(true);
 		outerMoon.setFilled(true);
 		innerMoon.setFilled(true);
 
+		// Fill the objects with colors
 		blueRect.setFillColor(AZ_BLUE);
 		redRect.setFillColor(AZ_RED);
 		greenRect.setFillColor(AZ_GREEN);
 		outerMoon.setFillColor(Color.WHITE);
 		innerMoon.setFillColor(AZ_RED);
 
+		// Remove borders
 		blueRect.setColor(AZ_BLUE);
 		redRect.setColor(AZ_RED);
 		blueRect.setColor(AZ_BLUE);
 		innerMoon.setColor(AZ_RED);
 		outerMoon.setColor(AZ_RED);
 
+		// Add to screen
 		add(blueRect);
 		add(redRect);
 		add(greenRect);
 		add(outerMoon);
 		add(innerMoon);
-
 	}
 
-
-
-
-
-
-
-
-	// Do not delete this:
 	public static void main(String[] args) {
 		new Task1_flag().start();
 	}
-
 }
